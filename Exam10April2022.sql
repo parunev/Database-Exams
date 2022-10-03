2e363ce4a7832f1d6e74fee4e8be046ab01f0524
Martin Parunev <parunev@gmail.com>
2022-10-03 16:05:10 +0300

    Update Exam10April2022.sql

--- Exam10April2022.sql
+++ Exam10April2022.sql
@@ -166,12 +166,12 @@ BEGIN
 
 CREATE PROCEDURE udp_award_movie(movie_title VARCHAR(50))
 BEGIN
-	UPDATE actors AS a
-	JOIN movies_actors AS ma
+    UPDATE actors AS a
+    JOIN movies_actors AS ma
     ON a.id = ma.actor_id
     JOIN movies AS m
     ON ma.movie_id = m.id
     SET a.awards = a.awards + 1
-	WHERE m.title = movie_title;
+    WHERE m.title = movie_title;
 END;
 
